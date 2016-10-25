7347522a4c1c20774c7a7b93ff51e52c07bb3526
Trisha Funtanilla <tfuntani@cisco.com>
2016-10-25 14:08:35 -0700

    extended ui flow

--- public/app.js
+++ public/app.js
@@ -3,18 +3,21 @@ import uiRoutes from 'ui/routes';
 
 import 'ui/autoload/styles';
 
-import listOfIndicesPage from './templates/index.html';
+import indexPage from './templates/index.html';
 import mainPage from './templates/main.html';
 import jsonFormatter from 'jsonformatter';
 
 import _ from 'lodash';
 import 'angular-ui-bootstrap';
 
+// Usually, this is added to the HTML, but not sure how to do that here in Kibana
+import './css/json-formatter.css';
+
 uiRoutes.enable();
 uiRoutes
 .when('/', {
-  template: listOfIndicesPage,
-  controller: 'listOfIndicesPageController',
+  template: indexPage,
+  controller: 'indexPageController',
   controllerAs: 'ctrl'
 })
 .when('/index/:name', {
@@ -26,29 +29,38 @@ uiRoutes
 
 uiModules
 .get('app/log_engine', ['ui.bootstrap', 'jsonFormatter'])
-.controller('listOfIndicesPageController', function ($http) {
+.controller('indexPageController', function ($http) {
 
-  $http.get('../api/log_engine/indices').then((response) => {
+  $http.get(`../api/log_engine/indices`).then((response) => {
     this.indices = response.data;
   });  
 
 })
 .controller('mainPageController', function ($routeParams, $http, $uibModal) {
 
-  // TODO
-  // call fetch entities API
-  // put all saved entities in an array
 
   this.index = $routeParams.name;
+
+  // TODO
+  // call fetch entities API
+  // put all saved entities' name in an array
+  this.app = "ClipOnClip";
+  this.fetchedEntities = ['entity name 1', 'entity name 2'];
+
+  $http.post(`http://127.0.0.1:8080/clip/entity/fetch?app=${this.app}`).then((response) => {
+    // this.fetchedEntities = ...
+    console.log(response);
+  }); 
 
   // search for entity via query
   this.getResults = function() {
 
     // TODO 
     // call API for querying via entity name
-    // $http.get(`...`).then((response) => {
-    //   this.entities = data;
-    // };
+
+    $http.get(`../api/log_engine/index/bank`).then((response) => {
+       this.entities = response.data;
+    });
 
   };
 
@@ -58,7 +70,7 @@ uiModules
 
   this.animationsEnabled = true;
 
-  this.createNewEntity = function(size) {
+  this.createNewEntity = function() {
 
     let $scope = this;
 
@@ -117,7 +129,37 @@ uiModules
           $uibModalInstance.dismiss('cancel');
         };
       },
-      size: size,
+      size: 'lg',
+      resolve: { }
+    });
+
+    modalInstance.result.then(function() {
+      console.log('Modal dismissed at: ' + new Date());
+    });
+
+  };
+
+  // on click of link
+  this.join = function() {
+
+    let $scope = this;
+
+    var modalInstance = $uibModal.open({
+      animation: this.animationsEnabled,
+      templateUrl: 'joinModal.html',
+      controller: function($scope, $uibModalInstance) {
+
+        $scope.execJoin = function() {
+
+          // TODO
+
+        }
+
+        $scope.cancel = function() {
+          $uibModalInstance.dismiss('cancel');
+        }
+
+      },
       resolve: { }
     });
 
